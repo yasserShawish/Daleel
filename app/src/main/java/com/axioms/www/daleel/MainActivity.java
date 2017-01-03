d03abad4a3009b8a254e6eb064b970e947f256f2
a7mad3babneh <a7mad3babneh17@yahoo.com>
2017-01-04 00:21:25 +0100

    remove the Toast for testing

--- app/src/main/java/com/axioms/www/daleel/MainActivity.java
+++ app/src/main/java/com/axioms/www/daleel/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Toast.makeText(getApplicationContext(), "Welcome ahmad Ababneh", Toast.LENGTH_SHORT).show();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
